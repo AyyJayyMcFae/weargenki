762165b1afec7c3f6b6229b0f7e4527943ec7159
AyyJayyMcFae <45824837+AyyJayyMcFae@users.noreply.github.com>
2026-03-16 11:37:40 -0500

    Update auth.js

--- scripts/auth.js
+++ scripts/auth.js
@@ -355,7 +355,7 @@ if (!authState.user) authState.isVanguard = false;
       body: JSON.stringify({ email: session.user.email, _subject: 'New Genki Account Created' }),
     });
   }
-  if (_event === 'SIGNED_IN' || _event === 'INITIAL_SESSION') window.updateAuthUi();
+  if (_event === 'SIGNED_IN' || _event === 'INITIAL_SESSION' || _event === 'TOKEN_REFRESHED') window.updateAuthUi();
 });
   await window.refreshAuthState();
 };
